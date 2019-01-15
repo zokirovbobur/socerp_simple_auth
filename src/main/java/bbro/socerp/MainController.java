f049f1cb3e3db5549f1d495d5a5f0d28b24b9b1e
zokirovbobur <33698387+zokirovbobur@users.noreply.github.com>
2019-01-15 14:08:39 +0500

    testing main controller

--- src/main/java/bbro/socerp/MainController.java
+++ src/main/java/bbro/socerp/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @RequestMapping("/")
     public String hw(){
-        return "Hello world";
+        return "Hello world from BBro";
     }
 }
